83ea90ceadae5183f4108991082ae2c8467f513f
chay <chay@chay-HP-240-14-inch-G9-Notebook-PC>
2024-11-27 15:18:39 -0600

    Backend V3 (sin validaciones)

--- src/Controlador/UserOrAdmin.java
+++ src/Controlador/UserOrAdmin.java
@@ -2,6 +2,8 @@
 package Controlador;
 
 import Modelo.Conexion; //Importamos la capa modelo
+import static Modelo.Conexion.queryInventary;
+import Vista.Inventario;
 import java.util.ArrayList;
 import java.sql.*;
 import javax.swing.JOptionPane;
@@ -53,4 +55,43 @@ public class UserOrAdmin {
         }
         return adminOuser;
     }
+    
+    public static Object[] consultaInv(){
+       //Object [] inv1 = {}; 
+       int cont=0;
+        Object [] inv = {};
+        ResultSet rs=queryInventary();
+        try{
+            while(rs.next()==true){
+                inv = new Object[]{rs.getString("id"),rs.getString("tipo"),rs.getString("descripcion"),
+                    rs.getString("cantidad"),rs.getString("marca"),rs.getString("modelo"),rs.getString("num_serie"),
+                    rs.getString("precio"),rs.getString("nombre")};
+                  Inventario.modeloInventario.addRow(inv);
+                  Inventario.tableInventario.setModel(Inventario.modeloInventario);
+            }
+            return inv;
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return inv;
+    }
+    
+    public static void deleteCar(int idcar){
+        boolean b=Conexion.querydelete("automovil",idcar);
+        if(b){
+            Inventario.btnConsulta.doClick();
+        }
+    }
 }
+//    CREATE TABLE automovil (
+//    id SERIAL PRIMARY KEY,           
+//    tipo VARCHAR(50) NOT NULL,       
+//    descripcion TEXT,                 
+//    cantidad INT,                     
+//    marca VARCHAR(50) NOT NULL,       
+//    modelo VARCHAR(50) NOT NULL,      
+//    num_serie VARCHAR(50) UNIQUE,     
+//    precio NUMERIC(10, 2),
+//	nombre VARCHAR(35)
+//);
